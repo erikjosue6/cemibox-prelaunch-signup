72329352475984172e40f0ea67d046611f381f5b
Erik J. Santiago <erik.santiago@cemi-tech.com>
2013-10-07 14:24:11 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CemiboxPrelaunchSignup::Application.config.secret_token = '1a01f76cd9841bef303f78f3f8699a56e54fec3eaddb9c7b615f2d8724505f3b159ca9490ec1e7b7b9aa66f5c579f26d11124a3acd0675705bac4732b002e39d'
