70401e42de7458636567e51fa063da98b8e8b58a
Erik J. Santiago <erik.santiago@cemi-tech.com>
2013-10-14 21:26:52 -0400

    Added gmail to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,23 +59,37 @@ CemiboxPrelaunchSignup::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify  
+ # config.active_support.deprecation = :notify  
 
-    config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 587,
-      :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_API_KEY"]
-    }
+  #  config.action_mailer.smtp_settings = {
+   #   :address   => "smtp.mandrillapp.com",
+    #  :port      => 587,
+    #  :user_name => ENV["MANDRILL_USERNAME"],
+     # :password  => ENV["MANDRILL_API_KEY"]
+   # }
 
+config.action_mailer.default_url_options = { :host => 'cemibox.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "myapp.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
-  config.action_mailer.default_url_options = { :host => 'cemibox.com' }
+ # config.action_mailer.default_url_options = { :host => 'cemibox.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default :charset => "utf-8"
 
 
   # Log the query plan for queries taking more than this (works
